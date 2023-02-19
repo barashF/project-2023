de9ea5e0436c950a3e1a3cadc3d99a2f020d0716
barashF <108573558+barashF@users.noreply.github.com>
2023-02-20 00:17:52 +0300

    фикс верстки

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django import forms
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class CreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+class UserRegistrationForm(ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
         model = User
-        fields = ("first_name", "last_name", "username", "email")
+        fields = ('username', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+    def clean_username(self):
+        name = self.cleaned_data
+        if name['username']:
+            raise forms.ValidationError('Passwords don\'t match.')
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from .forms import CreationForm
+from .forms import UserRegistrationForm
 
-class SignUp(CreateView):
-    form_class = CreationForm
-    success_url = reverse_lazy("login")
-    template_name = "signup.html"
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return redirect("market")
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'signup.html', {'user_form': user_form})
 # Create your views here.
